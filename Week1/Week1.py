fc88dd4f3864bf941b477a1500d12c97b2302522
Jkim2308 <107081194+Jkim2308@users.noreply.github.com>
2022-09-08 17:31:33 -0500

    updated Week1.py

--- Week1/Week1.py
+++ Week1/Week1.py
@@ -1,10 +1,8 @@
-
-def largestGap():
-    arr = [-3, -1, 6, 7, 0]
+def largestGap(arr):
     arr.sort()
-    print(arr)
-    print(arr[len(arr)-1] - arr[0])
+    print (arr[len(arr)-1] - arr[0])
 
-largestGap()
-
-
+largestGap([3, 10, 6, 7])
+largestGap([-3, -1, 6, 7, 0])
+largestGap([90, 10, 30, 50, 40])
+largestGap([-10, 5, 25, 15, -5])
